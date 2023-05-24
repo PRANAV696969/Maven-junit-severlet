3ebdebe9aaf7e2e66ec664fcf28520a12d6f9685
PRANAV696969 <107709470+PRANAV696969@users.noreply.github.com>
2023-05-24 11:49:35 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Build the Docker image
-docker build -t myapp:${TAG:-latest} -f Dockerfile .
+# Replace the placeholder with the actual build number
+sed -i "s/\${BUILD_NUMBER}/$BUILD_NUMBER/g" docker-compose.yml
 
-# Bring up the Docker Compose services
-docker-compose up -d --scale web=3
-
-# Gradually stop and remove the older containers
-OLD_CONTAINERS=$(docker ps -q -f ancestor=myapp:${TAG:-latest} --format '{{.Names}}' | sort -r)
-for CONTAINER in $OLD_CONTAINERS; do
-  docker-compose stop $CONTAINER
-  docker-compose rm -f $CONTAINER
-done
+# Run docker-compose
+docker-compose up -d
